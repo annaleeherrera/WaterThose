aa94cc02a1ab226c4f12e082eec28e265d3f4b66
Annalee <annaleeherrera@gmail.com>
2016-02-27 13:35:30 -0800

    power is on for 5 seconds

--- luaexamples/pump.lua
+++ luaexamples/pump.lua
@@ -1,17 +1,28 @@
+http = require("http")
+pumpOnTime = 5000
+pollingInterval = 500
 function polling()
-  require("http").get("http://waterthose.herokuapp.com/should_water", nil, function(code, data)
+  http.get("http://waterthose.herokuapp.com/should_water", nil, function(code, data)
     if (code == 200 and data == "Yes") then
       gpio.write(3, gpio.HIGH)
       print("Turning pump ON")
+      tmr.alarm(1,pumpOnTime,0,function ()
+        gpio.write(3, gpio.LOW)
+        print("Turning pump OFF")
+        polling()
+      end)
+
     else
       if (code ~= 200) then
         print(data,code)
       end
+
       gpio.write(3, gpio.LOW)
       print("Turning pump OFF")
+      tmr.alarm(3,pollingInterval,0,polling)
     end
   end)
 end
 
 gpio.mode(3, gpio.OUTPUT)
-tmr.alarm(3,1000,1,polling)
+polling()
