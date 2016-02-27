cd49869d68e368c1796c87166585b301b52988ff
Annalee <annaleeherrera@gmail.com>
2016-02-26 16:38:00 -0800

    polling working

--- luaexamples/test.lua
+++ luaexamples/test.lua
@@ -1,8 +1,9 @@
 tmr.alarm(3,1000,1,function()
   require("http").get("http://waterthose.herokuapp.com/should_water", nil, function(code, data)
     if (code < 0) then
-     print("HTTP request failed")
+      print("HTTP request failed")
     else
-    print(code, data)
+      print(code, data)
+    end
   end)
 end)
