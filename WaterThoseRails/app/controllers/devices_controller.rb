66a02a3cd7c8bfc6d6e23a7d86d9043f74c6bb62
Annalee <annaleeherrera@gmail.com>
2016-03-04 14:29:31 -0800

    bootstrap framework and first attempt at new methods in devices controller

--- WaterThoseRails/app/controllers/devices_controller.rb
+++ WaterThoseRails/app/controllers/devices_controller.rb
@@ -1,26 +1,44 @@
 class DevicesController < ApplicationController
   #Device client
+  #pass a post request upon plug in w/ assigning an id
+  before_action :find_device
+  before_action :assign
+
   def info
     device = Device.create(mac_address:'123abc')
     #insert value from lua code for mac_address
     device_should_water_url(device)
   end
 
+  def assign
+    # device = Device.where(id: device_params[:id])
+    if device = nil
+      device = Device.create
+    end
+  end
+
   def should_water
     device = Device.where(id: device_params[:id])
     #if device_id is nil, assign an id
+    #device.create
     if device.should_water == false
       render plain: "No"
     elsif device.should_water == true
       render plain: "Yes"
+      flash.now[:alert] = "Thanks, #{user.username}!"
+      redirect_to root_path, status: 200
     end
   device.should_water = false
   end
 
   #User client
   def water
-    device = Device.where(id: device_params[:device_id])
+    # device = Device.where(id: device_params[:device_id])
     device.should_water = true
+  end
+
+  def find_device
+    device = Device.find_by(params[:id])
   end
 
   def device_params
--- WaterThoseRails/app/models/user.rb
+++ WaterThoseRails/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-
+  has_many :devices
+  
   def create
     user = User.create
   end
