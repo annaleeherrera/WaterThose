8052d2b538ac4664c7a872b2aa36db14b2a2dc90
Annalee <annaleeherrera@gmail.com>
2016-03-15 13:37:41 -0700

    front page edit, need to take out login

--- WaterThoseRails/app/assets/javascripts/users.js
+++ WaterThoseRails/app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ $(function(){
   $(".water_button").click(function(){
     var mac_address= this.getAttribute('data-mac-address');
     $.post('/devices/water?mac_address='+mac_address).then(function() {
-      alert('OK');
+      alert('Your plant has been watered!');
     });
   });
 });
