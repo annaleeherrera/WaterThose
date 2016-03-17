e919bfcce479f58f0cb4d3208d5f6eedab10ba81
Annalee <annaleeherrera@gmail.com>
2016-03-17 13:51:32 -0700

    deleted nil in database

--- WaterThoseRails/app/helpers/devices_helper.rb
+++ WaterThoseRails/app/helpers/devices_helper.rb
@@ -1,5 +1,5 @@
 module DevicesHelper
   def device_water_time(hour_utc)
-    "#{(Date.today + hour_utc.hours).in_time_zone(-7).strftime("%I:00 %p")}"
+    "#{(Date.today + hour_utc.hours).in_time_zone(-7).strftime("%l:00 %p")}"
   end
 end
